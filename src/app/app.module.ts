65b9f6e9f3738a1c4a65a88d4cca6402bee210c3
K-Vishwak <vishwak.kurapati@gmail.com>
2022-01-30 17:47:11 +0530

    add header footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FiltersComponent } from './filters/filters.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from '.common/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FiltersComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
