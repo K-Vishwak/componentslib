2132a9101fceb129651b5d30f7b302660fa441a0
K-Vishwak <vishwak.kurapati@gmail.com>
2022-01-31 10:14:02 +0530

    enable basic route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FiltersComponent } from './filters/filters.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'filters',
+    component: FiltersComponent
+  },
 ];
 
 @NgModule({
